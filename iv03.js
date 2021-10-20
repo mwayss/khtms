79ae0f1ec722e6f6e4a7894b724ff87839514e0d
mwayss <92724834+mwayss@users.noreply.github.com>
2021-10-20 13:50:54 +0300

    Create iv03.js

---
+++ iv03.js
@@ -0,0 +1,12 @@
+try {
+    g = document.createElement('div');
+    g.setAttribute("id", "mox-4e5df3e2b4c8aa732ceaea194b6a68cc1");
+    document.body.appendChild(g);
+    let mscript = document.createElement("script");
+    mscript.src = "https://ad.mox.tv/mox/mwayss_invocation.min.js?pzoneid=6529&height=405&width=720&tld=khaleejtimes.com&ctype=div";
+    g.parentNode.insertBefore(mscript, g);
+} catch(err) {
+
+    console.log("Issue with apply mox video");
+
+}
