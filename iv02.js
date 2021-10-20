5c59cf565975969326eda619e1681f7e26005e6e
mwayss <92724834+mwayss@users.noreply.github.com>
2021-10-20 13:52:01 +0300

    Update iv02.js

--- iv02.js
+++ iv02.js
@@ -1,23 +1,15 @@
 try {
-
     let mscript = document.createElement("script");
-
     mscript.src = "https://ad.mox.tv/vidverto/js/aries/v1/invocation.js";
-
     var mnode = document.getElementsByTagName("script")[0];
-
     mnode.parentNode.insertBefore(mscript, mnode);
-
     g = document.createElement('div');
     g.setAttribute("id", "mox-4e5df3e2b4c8aa732ceaea194b6a68cc");
     document.body.appendChild(g);
     setTimeout(function() {
-
         window.aries = window.aries || {};
         window.aries.v1 = window.aries.v1 || {commands: []};
-
         const aries = window.aries.v1;
-
         aries.commands.push(() => {
           const zoneId = '6529';
           const anchor = document.getElementById('mox-4e5df3e2b4c8aa732ceaea194b6a68cc');
@@ -27,9 +19,6 @@ try {
           });
         });
     }, 1000);
-
 } catch(err) {
-
     console.log("Issue with apply mox video");
-
 }
